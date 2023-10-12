92fa62f67408bec6b2c1849f72e7161a98da19e7
Flgjfvevdk <74116706+Flgjfvevdk@users.noreply.github.com>
2023-10-12 17:33:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
 
-
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
